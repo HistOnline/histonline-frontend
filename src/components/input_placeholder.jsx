1d856d9394f794dddbef1d67d389c727307cadc9
Filipe Lopes <filiperocklopes@gmail.com>
2019-12-27 19:45:01 -0300

    Alterado componente de input de login, simplificando-os. Transformando componente de login em hooks e atribuido novo endpoint

--- src/components/input_placeholder.jsx
+++ src/components/input_placeholder.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 
 class InputPlaceholder extends Component {
-    constructor(props){
-        super(props);
 
-        this.state = {
-            isempty: true,
-        }
-    }
-
+    state = { isempty: true }
+    
+    /**
+     * Verifica se o input está cheio para 
+     * assim poder atribuir classe de estilo
+     */
     onChangeFunc = (input, value) => {
-        console.log("funcao 1");
-        // Verifica se está cheio para colocar state filled 
-        if(value === ""){
-            input.className = ""
-        }else{
-            input.className = "filled"
-        }
-        //passa valor para o pai
-        console.log("funcao 2");        
-        this.props.getValue(value)
+        input.className = value === '' ? '' : 'filled'
     }
 
     render(){
         return (        
         <div className="placeholder_input">
-            <input type={this.props.type} id={this.props.id} onChange={event => this.onChangeFunc(event.target, event.target.value)} className={this.state.inputClass} />
+            <input type={this.props.type} id={this.props.id} onChange={this.props.onChange ? e => {
+                this.props.onChange(e)
+                this.onChangeFunc(e.target, e.target.value)
+            } : null } className={this.state.inputClass} />
             <label htmlFor={this.props.id}>{this.props.placeholder}</label>
         </div>
         );
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,89 +1,64 @@
-import React, { Component } from 'react';
-import InputPlaceholder from './input_placeholder';
-import logo from '../img/logo_white.png';
+import React, { useState } from 'react'
+import InputPlaceholder from './input_placeholder'
+import logo from '../img/logo_white.png'
+import axios from 'axios'
 
-const aviso = (info) => {
-  console.log(info);
-  
-  return(
-    <div id="aviso">{info}</div>
-  )
+/**
+ * Componente de aviso na tela de login, inicialmente
+ * @param {string} info 
+ */
+export const Aviso = info => {
+  console.log('Aviso:', info);
+  return <div id="aviso">{info}</div>
 }
 
-class Login extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      username: "none",
-      password: "none"
-    }
-  }
+const Login = ({error}) => {
 
-  submit(){
-    const username = this.state.username;
-    const password = this.state.password;
+  const [user, setUser] = useState(null)
+  const [pass, setPass] = useState(null)
 
-    fetch(`${process.env.REACT_APP_API}/auth` , {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-      },
-      body: JSON.stringify({
-        username,
-        password
+  const submit = () => {
+
+    axios.post(`${process.env.REACT_APP_API}/users/v1/auth`, {
+      user,
+      pass
+    })
+      .then(function (response) {
+        const token = response.data.hash;
+        if (token) localStorage.setItem("histonline_token", token);
+        if (token) window.location.href = "./";
       })
-    })
-    .then(function(data) {
-      console.log(data);  
-      return data.text();
-    }).then(function(jwt){      
-      const token = JSON.parse(jwt).access_token;
-      if(token) localStorage.setItem("histonline_token", token);
-      if(token) window.location.href = "./";
-    })
+      .catch((err) => console.log('Erro de login:', err))
   }
 
-  getUsername = (username) => {
-    this.setState({ username }, () => {
-      console.log(this.state.username); 
-    });    
-  }
-
-  getPassword = (password) => {
-    this.setState({ password }, () => {
-      console.log(this.state.password); 
-    });    
-  }
-
-  render (){
-    return(
-      <section id="loginPage">
-        <div className="login_box">
-          <header className="App-header">
-          <img alt="Logo HistOnline" src={logo}/>
-          </header>
-          <section>
-            <InputPlaceholder type="text" placeholder="Digite seu usuário ou email" id="usuario" getValue={this.getUsername}/>
-            <InputPlaceholder type="password" placeholder="Digite sua senha" id="senha" getValue={this.getPassword} />
-            <div className="alignRight">
-            <button type="submit" onClick={event => this.submit(event)}>Entrar</button>
-            </div>
-          </section>
-          <p>
+  return (
+    <section id="loginPage">
+      <div className="login_box">
+        <header className="App-header">
+          <img alt="Logo HistOnline" src={logo} />
+        </header>
+        <section>
+          <InputPlaceholder type="text" placeholder="Digite seu usuário ou email" id="usuario" onChange={e => setUser(e.target.value)} />
+          <InputPlaceholder type="password" placeholder="Digite sua senha" id="senha" onChange={e => setPass(e.target.value)} />
+          <div className="alignRight">
+            <button type="submit" onClick={submit}>Entrar</button>
+          </div>
+        </section>
+        <p>
           Breve um atlas digital para você!
           </p>
-        </div>
+      </div>
 
-        <div className="whatsthis just_desktop">
-          <h3>O que é isso?</h3><br/>
-          <span>Descrição da lâmina com curiosidades e principais características em foco.</span>
-        </div>
+      <div className="whatsthis just_desktop">
+        <h3>O que é isso?</h3><br />
+        <span>Descrição da lâmina com curiosidades e principais características em foco.</span>
+      </div>
 
-        { this.props.error ? aviso(this.props.error) : null }
-        
-        </section>
-    )
-  }
+      {error ? Aviso(error) : null}
+
+    </section>
+  )
+
 }
 
 export default Login;
