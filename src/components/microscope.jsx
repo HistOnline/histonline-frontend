1bdcf7a00162404e59bb1ddbfecb43d58b56f588
Filipe Lopes <filiperocklopes@gmail.com>
2020-01-10 05:53:21 -0300

    Segunda tentativa de correção de posicionamento de máscaras

--- src/components/microscope.jsx
+++ src/components/microscope.jsx
@@ -3,24 +3,14 @@ import Zoom from './zoom';
 import axios from 'axios'
 import { Div } from '../styles/microscope'
 
-const Svg = ({ svg }) => {
-
-  // console.log('SVG', svg)
-
-  if (!svg) {
-    return "Sem arquivo de máscara"
-  }
-  return <Div
-    dangerouslySetInnerHTML={{ __html: svg }}
-    className="svg_mask"
-    // color={this.props.color}
-  />
-}
-
 export default class Microscope extends Component {
 
   state = {
     mascaras: []
+  }
+
+  resetMasks = () => {
+    this.setState({ mascaras: [...this.props.lamina.mascaras] }, this.loadMasks)
   }
 
   /**
@@ -29,9 +19,9 @@ export default class Microscope extends Component {
    * @param {string} url 
    * @param {integer} index 
    */
-  getSVG = (url, index) => {
+  loadMask = (url, index) => {
 
-    console.log('novo svg')
+    // carrega nova máscara caso a máscara não exista
     fetch(url)
       .then(res => res.text())
       .then(text => {
@@ -39,77 +29,70 @@ export default class Microscope extends Component {
         mascaras[index].svg = text
         this.setState({ mascaras }, () => console.log('mascaras', this.state.mascaras))
       });
+  }
 
+  loadMasks = () => {
+    let mascaras = this.state.mascaras
+    mascaras.map(({ vector }, index) => {
+      this.loadMask(vector, index)
+    })
   }
 
   componentDidMount() {
-    // console.log('montou')
-    // let mascaras = this.props.lamina.mascaras,
-    //   tmpMascaras = []
-    // mascaras.map(mascara => {
-    //   tmpMascaras.push({
-    //     url: mascara.vector,
-    //     svg: null
-    //   })
-    // })
-    
-    // this.setState({ mascaras: tmpMascaras }, () => {
-    //   this.state.mascaras.map( (mascara, index) => {
-    //     this.getSVG( mascara.url, index)
-    //   })
-    // })
+    this.resetMasks()
   }
 
-  shouldComponentUpdate(nextProps, nextState){
-    console.log('verificando se tem que atualizar')
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Verificando atualização')
+
     // Verificar se o slug da lâmina deve mudar
-    console.log(this.props.lamina.ID === nextProps.lamina.ID)
-    if(this.props.lamina.ID === nextProps.lamina.ID){
-      // Verificar se existe state.mascaras ou se está igual ao antigo ainda
-      if(this.state.mascaras.length !== nextState.mascaras.length) return true
-      this.state.mascaras.map( mascara => {
-        if(!mascara.svg) return true
-      })
-      return false
+    console.log('CONDICIONAL 1', this.props.lamina.ID !== nextProps.lamina.ID)
+    if (this.props.lamina.ID !== nextProps.lamina.ID) {
+      this.resetMasks()
+      return true
     }
-    console.log('states', this.state.mascaras, nextState.mascaras)
 
-    // Verifica se algum svg do state.mascaras
+    // Verificar se existe state.mascaras ou se está igual ao antigo ainda
+    console.log('CONDICIONAL 2', this.state.mascaras.length !== nextState.mascaras.length)
+    if (this.state.mascaras.length !== nextState.mascaras.length) return true
 
-    return true
+    let emptySVG = false
+    this.state.mascaras.map(mascara => {
+      if (!mascara.svg) emptySVG = true
+    })
+    console.log('CONDICIONAL 3', emptySVG)
+    if (emptySVG) return true
+
+    console.log('CONDICIONAL ÚLTIMA', false)
+    return false
   }
 
-  componentDidUpdate(){
-    let mascaras = this.props.lamina.mascaras,
-      tmpMascaras = []
-      if(mascaras){
-        mascaras.map(mascara => {
-          tmpMascaras.push({
-            url: mascara.vector,
-            svg: null
-          })
-        })
-        
-        this.setState({ mascaras: tmpMascaras }, () => {
-          this.state.mascaras.map( (mascara, index) => {
-            this.getSVG( mascara.url, index)
-          })
-        })
-      }
+  componentDidUpdate(prevProps) {
+    console.log('chamando UPDATE')
+    if (prevProps.lamina.ID !== this.props.lamina.ID) {
+      console.log('RESETA')
+      this.resetMasks()
+    } else {
+      console.log('só CARREGA')
+      this.loadMasks()
+    }
   }
 
   render() {
-
-    let lamina = this.props.lamina
+    console.log('chamando RENDER')
 
     return (
       <section id="microscope">
-        {lamina ? <div id="microscope_imgs">
-          <img src={lamina.imagem[0]} />
-          {this.state.mascaras ? this.state.mascaras.map( mascara => {
-            return <Svg svg={mascara.svg} color={mascara.color} />
+        {this.props.lamina ? <div id="microscope_imgs">
+          <img src={this.props.lamina.imagem[0]} />
+          {this.state.mascaras ? this.state.mascaras.map(({ svg, color }) => {
+            return svg ? <Div
+              dangerouslySetInnerHTML={{ __html: svg }}
+              className="svg_mask"
+              color={color}
+            /> : 'Carregando...'
             // return mascara.title
-          }) : null }
+          }) : null}
           {/* <Zoom lens="200" href="lamina_y_200" top="45" left="45" /> */}
         </div> : null}
       </section>
