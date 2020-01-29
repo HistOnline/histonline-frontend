d72365a9c460f3ca17edd86fed7b8c0b3b6e5765
Filipe Lopes <filiperocklopes@gmail.com>
2020-01-28 23:52:33 -0300

    Criado context de lâmina

--- src/views/Lamina/index.jsx
+++ src/views/Lamina/index.jsx
@@ -7,13 +7,28 @@ import Microscope from './components/Microscope';
 import Description from './components/Description';
 import Footer from '../../components/Footer';
 
+import LaminaContext from './context'
 import Laminas from '../../services/crud/laminas';
 
 import { KeyboardArrowDown } from '@material-ui/icons';
 
-const Lamina = props => {
 
+const Lamina = props => {
+  
   const [lamina, setLamina] = useState(null)
+  
+  const context = {
+    lamina: null,
+    count: 0
+  }
+
+  // Não funciona, transformar em classe
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     context.count = context.count+1
+  //     console.log(context.count)
+  //   }, 1000)
+  // }, [])
 
   useEffect(() => {
     const slug = props.match.params.slug
@@ -30,15 +45,18 @@ const Lamina = props => {
         <main>
           <Aside />
           <section id="page" className={`wrap`}>
-            {lamina ? [
-              <section>
-                <img alt="Logo" src={logo} className="logo" />
-                <Microscope lamina={lamina}/>
-                <h3>ID: {props.match.params.slug}</h3>
-                <span className="scroll_message"><KeyboardArrowDown /> Role a página para mais informações</span>
-              </section>,
-              <Description lamina={lamina}/>
-            ] : "Carregando..."}
+            <LaminaContext.Provider value={context}>
+              {context.count}
+              {lamina ? [
+                <section>
+                  <img alt="Logo" src={logo} className="logo" />
+                  <Microscope lamina={lamina} />
+                  <h3>ID: {props.match.params.slug}</h3>
+                  <span className="scroll_message"><KeyboardArrowDown /> Role a página para mais informações</span>
+                </section>,
+                <Description lamina={lamina} />
+              ] : "Carregando..."}
+            </LaminaContext.Provider>
           </section>
         </main>
         <Footer />
