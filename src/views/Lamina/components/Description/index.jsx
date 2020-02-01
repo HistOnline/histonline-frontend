971e2a4bb56850fb37286ac6e3a6fcfb8ad3a420
Filipe Lopes <filiperocklopes@gmail.com>
2020-02-01 16:02:33 -0300

    Adicionado condicional para permitir visualização de lâminas sem máscara

--- src/views/Lamina/components/Description/index.jsx
+++ src/views/Lamina/components/Description/index.jsx
@@ -5,23 +5,23 @@ const Description = ({ lamina }) => {
   const filteredDescription = description => {
     
     let masks = lamina.mascaras
+    if(masks){
+      masks.map( function({ alias }){
+        var regex = new RegExp(` @${alias} `,"gi")
+        var matches = description.match(regex)
+        console.log('matches', matches, regex)
+        if(matches){
+          matches.map( function( match ){
+            console.log(match)
+            description = description.replace(match, ` <span class="blue">${match.replace(' ', '')}</span> `)
+          })
+        }
+      })
+    }
   
-    masks.map( function({ alias }){
-      var regex = new RegExp(` @${alias} `,"gi")
-      var matches = description.match(regex)
-      console.log('matches', matches, regex)
-      if(matches){
-        matches.map( function( match ){
-          console.log(match)
-          description = description.replace(match, ` <span class="blue">${match.replace(' ', '')}</span> `)
-        })
-      }
-    })
-  
-    console.log('description', description)
     return description
   }
-  
+
   return (
     <section id="descricao_lamina">
       <h1>Tecido X</h1>
